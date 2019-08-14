d0f4c52e9096f87614d4daae71c958159a26cb77
spanda <spanda@tavisca.in>
2019-08-14 07:42:27 +0530

    Working Solution

---
+++ LeftOrRight/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeJam
+{
+    class LeftOrRight
+    {
+        int MaxDistance(string program)
+        {
+            
+            int left = 0, right = 0, leftMAx = 0, rightMax = 0;
+           
+
+            
+            foreach(char c in program)
+            {
+                if (c == 'L')
+                {
+                    
+                    left--;
+                    right--;
+                    if (left < leftMAx)
+                        leftMAx = left;
+                }
+                    
+                else if (c == 'R')
+                {
+                    
+                    left++;
+                    right++;
+                    if (right > rightMax)
+                        rightMax = right;
+                }
+
+                else
+                {
+                    left--;
+                    right++;
+                    if (left < leftMAx)
+                        leftMAx = left;
+                    if (right > rightMax)
+                        rightMax = right;
+                    
+                }
+
+               
+
+                    
+
+            }
+            if (leftMAx * -1 > rightMax)
+                return leftMAx * -1;
+            else
+                return rightMax;
+        }
+
+        #region Testing code Do not change
+        public static void Main(String[] args)
+        {
+            LeftOrRight leftOrRight = new LeftOrRight();
+            String input = Console.ReadLine();
+            do
+            {
+                Console.WriteLine(leftOrRight.MaxDistance(input));
+                input = Console.ReadLine();
+            } while (input != "-1");
+        }
+        #endregion
+    }
+    
+
+    
+}
